445fb2ac8dcd975a343b7bb71186037f0380a7f7
GH-Desktop <Yerel@TB-02.huuavs.net>
2018-01-30 08:12:26 +0300

    Various improvements

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 #
-# bittrex üzerindeki kriptoparaların dolar karşılığını veren basit bir program
+# A simple program to return USDT exchange rate of the entered cryptocurrency codes on bittrex.
 #
 
 import requests
 
-def sorgula():
-    veri = requests.get("https://bittrex.com/api/v1.1/public/getmarketsummaries")
-    while True:
-        isim = "USDT-" + input("USDT karşılığını görmek istediğiniz kriptopara kodunu yazın (Örn. : BTC, ETH)")
-        isim = isim.upper()
-        i=0
-        try:
-            while isim != veri.json()["result"][i]["MarketName"]:
-                i=i+1
-        except IndexError:
-            print("Girilen birimin USDT karşılığı bulunamadı.")
-        else: break
-    print(veri.json()["result"][i]["Last"])
-sorgula()
+def crypQuery():
+    try:
+        data_Raw = requests.get("https://bittrex.com/api/v1.1/public/getmarketsummaries")
+    except:
+        print("Connection to the server could not be established.")
+        newQuery()
+    crypName = "USDT-" + input("Enter the crptocurrency code that you wish to see USDT exchange rate (Ex. : BTC, ETH):")
+    crypName = crypName.upper()
+    resultCheck = False
+    for data_Markets in data_Raw.json()["result"]:
+        if data_Markets['MarketName'] == crypName:
+            print(data_Markets["Last"])
+            resultCheck = True
+            break
+    if resultCheck == False:
+        print("Currency code does not exist.")
 
-for n in range(3):
-    Cevab = input("Tekrar ara?(E/H)")
-    if Cevab == ('e' or 'E'):
-        sorgula()
-    elif Cevab == ('h' or 'H'):
-        break
-    else:
-        print("Hatalı giriş.")
-    continue
+    # print(type(data_Raw.json()["result"].index(data_Markets)))
+    # print(type(len(data_Raw.json()["result"])))
+
+    # if (data_Raw.json()["result"].index(data_Markets) == len(data_Raw.json()["result"]-1)):
+    #     print("not found")
+
+def newQuery():
+    for iteration in range(3):
+        Ans = input("New query?(Y/N)")
+        if Ans == ('y' or 'Y'):
+            crypQuery()
+            continue
+        elif Ans == ('n' or 'N'):
+            break
+        else:
+            print("Faulty input.")
+
+crypQuery()
+newQuery()
